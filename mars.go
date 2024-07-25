f13d0216fe3d2a03c62ab199dbfa2cb3c5f04998
166****4030 <18712723595@163.com>
2024-07-25 10:44:34 +0000

    2.6

--- mars.go
+++ mars.go
@@ -1,6 +1,11 @@
 package main
+
 import "fmt"
 import "math/rand"
-func main(){
-    
+
+func main() {
+	var num = rand.Intn(10) + 1
+	fmt.Println(num)
+	num = rand.Intn(19) + 1
+	fmt.Println(num)
 }
