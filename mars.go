b3d65fd3221435e1842d6e48cc5ee055034fb40d
166****4030 <18712723595@163.com>
2024-07-25 08:26:50 +0000

    2.2

---
+++ mars.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main(){
+    fmt.Printf("My weight on the surface of Mars is %v lbs,", 149.0*0.3783)
+    fmt.Printf("and I wound be %4v years old.\n", 41*365/687)
+    fmt.Printf("My weight on the surface of %s is %.2f lbs.\n", "Earth", 149.0)
+}
