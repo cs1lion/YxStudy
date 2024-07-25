867737db63e2085c081c79044228f9b1ab3f74b7
166****4030 <18712723595@163.com>
2024-07-25 13:31:31 +0000

    3.8

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "time"
+import "math/rand"
 
 func main() {
 	var count = 10
@@ -12,4 +13,20 @@ func main() {
 		count--
 	}
 	fmt.Println("Liftoff!")
+
+	//homework
+	var target = 43
+	var num int
+	for {
+		num = rand.Intn(100)
+		if num < target {
+			fmt.Println("lower")
+		} else if num > target {
+			fmt.Println("higher")
+		} else {
+			fmt.Println("bingo!")
+			break
+		}
+		time.Sleep(time.Second)
+	}
 }
