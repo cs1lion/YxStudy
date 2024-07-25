7053dc18aed9a3dfbe5809c36eaa649ce98ebc3a
166****4030 <18712723595@163.com>
2024-07-25 11:07:35 +0000

    3.2

--- mars.go
+++ mars.go
@@ -1,14 +1,13 @@
 package main
 
-import(
+import (
 	"fmt"
-	"strings"
 )
 
-
 func main() {
-	fmt.Println("You find yourself in a dimlt lit cavern.")
-	var command = "walk outside"
-	var exit = strings.Contains(command,"outside")
-	fmt.Println("You leave the cave",exit)
+	fmt.Println("There is a sign near the entrance that reads 'No Minors'.")
+
+	var age = 41
+	var minor = age < 18
+	fmt.Println("At age %v,am I a minor? %v\n", age, minor)
 }
