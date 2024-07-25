677f6d691e234f2600c26e42bfeea29383250728
166****4030 <18712723595@163.com>
2024-07-25 08:34:11 +0000

    2.2.1

--- mars.go
+++ mars.go
@@ -1,7 +1,6 @@
 package main
 import "fmt"
 func main(){
-    fmt.Printf("My weight on the surface of Mars is %v lbs,", 149.0*0.3783)
-    fmt.Printf("and I wound be %4v years old.\n", 41*365/687)
-    fmt.Printf("My weight on the surface of %s is %.2f lbs.\n", "Earth", 149.0)
+    fmt.Printf("%-15v $%4v\n", "SpaceX", 94)
+    fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
 }
