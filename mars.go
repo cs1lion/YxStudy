7a83593ba73c6cd0a60d223cfecd34b5e4ab38ad
166****4030 <18712723595@163.com>
2024-07-25 11:36:40 +0000

    3.4

--- mars.go
+++ mars.go
@@ -5,12 +5,13 @@ import (
 )
 
 func main() {
-	var command = "go east"
-	if command == "go east" {
-		fmt.Println("You head further up the mountain")
-	} else if command == "go inside" {
-		fmt.Println("You enter the cave where you live out the rest of your life")
+	fmt.Println("The year is 2100,should you leap?")
+	var year = 2100
+	var leap = year%400 == 0 || (year%4 == 0 && year%100 != 0)
+
+	if leap {
+		fmt.Println("Look before you leap!")
 	} else {
-		fmt.Println("Didn't quite get that")
+		fmt.Println("Keep your feet on the ground.")
 	}
 }
