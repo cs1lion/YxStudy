43bb040dcaa03b77c49b2580c19a6aaff050ca38
166****4030 <18712723595@163.com>
2024-07-25 14:29:48 +0000

    3.4.1

--- mars.go
+++ mars.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
+
+var era = "AD"
 
 func main() {
 	fmt.Println("The year is 2100,should you leap?")
@@ -14,4 +17,23 @@ func main() {
 	} else {
 		fmt.Println("Keep your feet on the ground.")
 	}
+	//homework展示随机日期
+	for count := 10; count > 0; count-- {
+		year := rand.Intn(2500) + 1
+		mouth := rand.Intn(12) + 1
+		daysInMouth := 31
+		switch mouth {
+		case 2:
+			if year%400 == 0 || (year%4 == 0 && year%100 != 0) {
+				daysInMouth = 29
+			} else {
+				daysInMouth = 28
+			}
+		case 4, 6, 9, 11:
+			daysInMouth = 30
+		}
+		day := rand.Intn(daysInMouth)
+		fmt.Println(era, year, mouth, day)
+	}
+
 }
