6d1366f08f84a4a4b88f5a816ed6984efa1a6075
166****4030 <18712723595@163.com>
2024-07-26 07:51:23 +0000

    4.1

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 //import "time"
-import "math/rand"
+import "math/big"
 
 // const(
 // 	Virgin Galactic = iota +1
@@ -12,35 +12,18 @@ import "math/rand"
 // )
 
 func main() {
-	const distance = 62100000
-	fmt.Printf("%-17s%-5v%-11v%v\n", "Spaceline", "Days", "Trip type", "Price")
-	fmt.Println("=======================================")
-	for count := 10; count > 0; count-- {
-		comynum := rand.Intn(3) + 1
-		comyname := ""
-		switch comynum {
-		case 1:
-			comyname = "Virgin Galactic"
-		case 2:
-			comyname = "SpaceX"
-		case 3:
-			comyname = "Space Adventures"
-		default:
-			comyname = "Unknown"
-		}
-		speed := rand.Intn(15) + 16
-		Days := (int)(distance / (speed * 3600 * 24))
-		tripnum := rand.Intn(2) + 1
-		triptype := ""
-		switch tripnum {
-		case 1:
-			triptype = "One-Way"
-		case 2:
-			triptype = "Round-trip"
-		}
-		pricerand := rand.Intn(15) + 36
-		price := pricerand * tripnum
-		fmt.Printf("%-17s%4v %-11v$ %3v\n", comyname, Days, triptype, price)
-	}
+	LightSpeed := big.NewInt(299792)
+	secondsPerDay := big.NewInt(86400)
 
+	distance := new(big.Int)
+	distance.SetString("24000000000000000000", 10)
+	fmt.Println("Andromeda Galaxy is", distance, "km away")
+
+	seconds := new(big.Int)
+	seconds.Div(distance, LightSpeed)
+
+	days := new(big.Int)
+	days.Div(seconds, secondsPerDay)
+
+	fmt.Println("That is", days, "days of travel at light speed")
 }
