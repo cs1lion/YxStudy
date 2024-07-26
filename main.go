2f3cbc6b9fa5e912e78d0d846b8497ea6ef4232c
166****4030 <18712723595@163.com>
2024-07-26 02:52:56 +0000

    Format

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+
 //import "time"
 import "math/rand"
 
@@ -12,36 +13,34 @@ import "math/rand"
 
 func main() {
 	const distance = 62100000
-	fmt.Printf("%-17s%-5v%-11v%v\n","Spaceline","Days","Trip type","Price")
+	fmt.Printf("%-17s%-5v%-11v%v\n", "Spaceline", "Days", "Trip type", "Price")
 	fmt.Println("=======================================")
-	for count:=10;count>0;count--{
-		comynum:=rand.Intn(3)+1
-		comyname:=""
-		switch comynum{
-			case 1:
+	for count := 10; count > 0; count-- {
+		comynum := rand.Intn(3) + 1
+		comyname := ""
+		switch comynum {
+		case 1:
 			comyname = "Virgin Galactic"
-			case 2:
+		case 2:
 			comyname = "SpaceX"
-			case 3:
+		case 3:
 			comyname = "Space Adventures"
-			default:
+		default:
 			comyname = "Unknown"
 		}
-		speed:=rand.Intn(15)+16
-		Days:=(int)(distance/(speed*3600*24))
-		tripnum:=rand.Intn(2)+1
-		triptype:=""
-		switch tripnum{
-			case 1:
+		speed := rand.Intn(15) + 16
+		Days := (int)(distance / (speed * 3600 * 24))
+		tripnum := rand.Intn(2) + 1
+		triptype := ""
+		switch tripnum {
+		case 1:
 			triptype = "One-Way"
-			case 2:
+		case 2:
 			triptype = "Round-trip"
 		}
-		pricerand:=rand.Intn(15)+36
-		price:=pricerand*tripnum
-		fmt.Printf("%-17s%4v %-11v$ %3v\n",comyname,Days,triptype,price)
+		pricerand := rand.Intn(15) + 36
+		price := pricerand * tripnum
+		fmt.Printf("%-17s%4v %-11v$ %3v\n", comyname, Days, triptype, price)
 	}
-		
 
-	
 }
