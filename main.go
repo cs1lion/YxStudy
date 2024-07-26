fb1d14ca5ee6aef6495fb89677c5fd6273391377
166****4030 <18712723595@163.com>
2024-07-26 02:04:03 +0000

    Quiz001

--- main.go
+++ main.go
@@ -1,32 +1,47 @@
 package main
 
 import "fmt"
-import "time"
+//import "time"
 import "math/rand"
 
+// const(
+// 	Virgin Galactic = iota +1
+// 	SpaceX
+// 	Space Adventures
+// )
+
 func main() {
-	var count = 10
-
-	for count > 0 {
-		fmt.Println(count)
-		time.Sleep(time.Second)
-		count--
-	}
-	fmt.Println("Liftoff!")
-
-	//homework
-	var target = 43
-	var num int
-	for {
-		num = rand.Intn(100)
-		if num < target {
-			fmt.Println("lower")
-		} else if num > target {
-			fmt.Println("higher")
-		} else {
-			fmt.Println("bingo!")
-			break
+	const distance = 62100000
+	fmt.Printf("%-17s%-5v%-11v%v\n","Spaceline","Days","Trip type","Price")
+	fmt.Println("=======================================")
+	for count:=10;count>0;count--{
+		comynum:=rand.Intn(3)+1
+		comyname:=""
+		switch comynum{
+			case 1:
+			comyname = "Virgin Galactic"
+			case 2:
+			comyname = "SpaceX"
+			case 3:
+			comyname = "Space Adventures"
+			default:
+			comyname = "Unknown"
 		}
-		time.Sleep(time.Second)
+		speed:=rand.Intn(15)+16
+		Days:=(int)(distance/(speed*3600*24))
+		tripnum:=rand.Intn(2)+1
+		triptype:=""
+		switch tripnum{
+			case 1:
+			triptype = "One-Way"
+			case 2:
+			triptype = "Round-trip"
+		}
+		pricerand:=rand.Intn(15)+36
+		price:=pricerand*tripnum
+		fmt.Printf("%-17s%4v %-11v$ %3v\n",comyname,Days,triptype,price)
 	}
+		
+
+	
 }
