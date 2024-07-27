404b5b693756d4299fed00ff1325b1b80852b924
166****4030 <18712723595@163.com>
2024-07-27 07:31:38 +0000

    5.1

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 //import "math"
 //import "strconv"
+
+func kelvinToCelsius(k float64) float64{
+        k-=273.15
+        return k
+    }
+
 func main(){
-    
+    kelvin:= 294.0
+    celsius:=kelvinToCelsius(kelvin)
+    fmt.Println(kelvin,"K is",celsius," C")
 }
