a59654626729d8f217b02313ce83f53adcd7e909
166****4030 <18712723595@163.com>
2024-07-27 01:32:42 +0000

    4.3.1

--- main.go
+++ main.go
@@ -1,27 +1,18 @@
 package main
 
 import "fmt"
+import "math"
+import "strconv"
+func main(){
+	v:=42
+	if v>=0&&v<=math.MaxUint8{
 
-//import "time"
-
-// const(
-// 	Virgin Galactic = iota +1
-// 	SpaceX
-// 	Space Adventures
-// )
-
-func main() {
-	message:="Lfdph,L vdz,L frqtxhuhg"
-	for _,c:=range message{
-		fmt.Printf("%c",c-3)
-
-		if c>='a'&&c<='z'{
-			c+=13
-			if c>'z'{
-				c-=26
-			}
-
-			//print
-		}
+	
+	v8:=uint8(v)
+	fmt.Println("converted:",v8)
 	}
+
+	intnum:=25649
+	fmt.Print("con:",string(intnum))
+	fmt.Print("con:",strconv.Itoa(intnum))
 }
