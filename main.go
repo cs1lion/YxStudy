8f4d0818af6deb4fa7e69d852b89361ac6a61f46
166****4030 <18712723595@163.com>
2024-07-27 08:17:25 +0000

    5.2

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import "fmt"
-//import "math"
-//import "strconv"
 
-func kelvinToCelsius(k float64) float64{
-        k-=273.15
-        return k
-    }
+type celsius float64
+
+func(c celsius) fahrenhelt() fahrenhelt{
+    return fahrenhelt((c*9.0/5.0)+32.0)
+}
+func(c celsius) kelvin() kelvin{
+    return kelvin(c+273.15)
+}
+
+type fahrenhelt float64
+
+func (f fahrenhelt) celsius() celsius{
+    return celsius((f-32.0)*5.0/9.0)
+}
+
+func(f fahrenhelt) kelvin() kelvin{
+    return f.celsius().kelvin()
+}
+
+type kelvin float64
+
+func(k kelvin) celsius() celsius{
+    return celsius(k-273.15)
+}
+
+func(k kelvin) fahrenhelt() fahrenhelt{
+    return k.celsius().fahrenhelt()
+}
 
 func main(){
-    kelvin:= 294.0
-    celsius:=kelvinToCelsius(kelvin)
-    fmt.Println(kelvin,"K is",celsius," C")
+    var f fahrenhelt = 255.2
+    fmt.Println("f:",f,"f to c:",f.celsius())
 }
