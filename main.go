77fb22be2f1e7b8c96f41f01839ced027f21cc41
166****4030 <18712723595@163.com>
2024-07-27 07:10:46 +0000

    Init

--- main.go
+++ main.go
@@ -4,19 +4,5 @@ import "fmt"
 //import "math"
 //import "strconv"
 func main(){
-    ciperText:= "CSOITEUIWUIZNSROCNKFD"
-    keyword:="GOLANG"
-    message:=""
-    keyIndex:=0
-    for i:=0;i<len(ciperText);i++{
-        //A=0,B=1....Z=25
-        c:=ciperText[i]-'A'
-        k:=keyword[keyIndex]-'A'
-        //加密字母-关键字(密码)字母
-        c = (c-k+26)%26+'A'
-        message+=string(c)
-        keyIndex++
-        keyIndex%=len(keyword)
-    }
-    fmt.Println(message)
+    
 }
