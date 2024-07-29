91a7420c0ad1a830e9ca1a3896abecf90fe09c64
166****4030 <18712723595@163.com>
2024-07-29 02:07:55 +0000

    6.2planets

--- main.go
+++ main.go
@@ -1,41 +1,17 @@
 package main
 
 import "fmt"
+type Planets []string
 
-func display(board [8][8]rune){
-    for _,row:=range board{
-        for _,column:=range row{
-            if column==0{
-                fmt.Print(" ")
-            }else{
-                fmt.Printf("%c",column)
-            }
-        }
-        fmt.Println()
+func (p Planets)terraform() {
+    for i,v:=range p{
+        p[i]=fmt.Sprintf("New %v",v)
     }
 }
 
 func main(){
-    var board [8][8]rune
-    board[0][0]='r'
-    board[0][1]='n'
-    board[0][2]='b'
-    board[0][3]='q'
-    board[0][4]='k'
-    board[0][5]='b'
-    board[0][6]='n'
-    board[0][7]='r'
-    for column:=range board[1]{
-        board[1][column]='P'
-        board[6][column]='p'
-    }
-    board[7][0]='R'
-    board[7][1]='N'
-    board[7][2]='B'
-    board[7][3]='Q'
-    board[7][4]='K'
-    board[7][5]='B'
-    board[7][6]='N'
-    board[7][7]='R'
-    display(board)
+    planets:=[]string{"a","b","Mars","c","Uranus","Neptune"}
+    Planets(planets[2:3]).terraform()
+    Planets(planets[4:]).terraform()
+    fmt.Print(planets)
 }
