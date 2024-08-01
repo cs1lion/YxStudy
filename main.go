3c089b4503e89685077397786a7bfc72916398b7
166****4030 <18712723595@163.com>
2024-08-01 09:30:11 +0000

    7.4 interface

--- main.go
+++ main.go
@@ -1,55 +1,70 @@
 package main
 
 
-import "fmt"
-import "math"
-type gps struct{
-    current location
-    destination location
-    world world
-}
-type location struct{
-    name string
-    lat,long float64
-}
-func(l location) description()string{
-    return fmt.Sprintf("%v (%.1f~,%.1f~)",l.name,l.lat,l.long)
-}
-type world struct{
-    radius float64
-}
-func (g gps) distance()float64{
-    return g.world.distance(g.current,g.destination)
-}
-func (g gps) message()string{
-    return fmt.Sprintf("%.1f km to %v",g.distance(),g.destination.description())
+import (
+    "fmt"
+    "encoding/json"
+    "os"
+)
+//使用度分秒格式的坐标表示半球
+type coordinate struct{
+    d,m,s float64
+    h rune
 }
 
-func (w world) distance(p1,p2 location) float64{
-    s1,c1:=math.Sincos(rad(p1.lat))
-    s2,c2:=math.Sincos(rad(p2.lat))
-    clong:=math.Cos(rad(p1.long-p2.long))
-    return w.radius*math.Acos(s1*s2+c1*c2*clong)
+func (c coordinate) String() string{
+    return fmt.Sprintf("%v~%v'%.1f\" %c",c.d,c.m,c.s,c.h)
 }
-func rad(deg float64)float64{
-    return deg*math.Pi/180
+//坐标转换为十进制度格式
+func (c coordinate) decimal() float64{
+    sign:=1.0
+    switch c.h{
+        case 'S','W','s','w':
+        sign=-1
+    }
+    return sign*(c.d+c.m/60+c.s/3600)
 }
-type rover struct{
-    gps
+
+func(c coordinate) MarshalJSON() ([]byte,error){
+    return json.Marshal(struct{
+        DD float64 `json:"decimal"`
+        DMS string `json:"dms"`
+        D float64 `json:"degrees"`
+        M float64 `json:"minutes"`
+        S float64 `json"seconds"`
+        H string `json:"hemisphere"`
+    }{
+        DD: c.decimal(),
+        DMS: c.String(),
+        D: c.d,
+        M: c.m,
+        S: c.s,
+        H: string(c.h),
+    })
+}
+
+type location struct{
+    Name string `json:"name"`
+    Lat coordinate `json:"latitude"`
+    Long coordinate `json:"longitude"`
 }
 
 func main(){
-    mars:=world{radius:3389.5}
-    bradbury:=location{"Bradbury Landing",-4.5895,137.4417}
-    elysium:=location{"Elysium Planitia",4.5,135.9}
-
-    gps:=gps{
-        world:mars,
-        current:bradbury,
-        destination:elysium,
+    elysium:=location{
+        Name:"Elysium Planitia",
+        Lat: coordinate{4,30,0.0,'N'},
+        Long: coordinate{135,54,0.0,'E'},
     }
-    curiosity:=rover{
-        gps:gps,
+    bytes,err:= json.MarshalIndent(elysium,""," ")
+    if err!=nil{
+        fmt.Println(err)
+        os.Exit(1)
     }
-    fmt.Println(curiosity.message())
+    fmt.Println(string(bytes))
+    //fmt.Println("=====================================")
+    //bytes,err=elysium.Lat.MarshalJSON()
+    //fmt.Println(string(bytes))
+    //fmt.Println("=====================================")
+    //bytes,err=elysium.Long.MarshalJSON()
+    //fmt.Println(string(bytes))
 }
