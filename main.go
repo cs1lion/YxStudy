5d87ed8bc49234cb7597fea000e83801ba76d60c
166****4030 <18712723595@163.com>
2024-08-03 07:44:53 +0000

    7.7 err

--- main.go
+++ main.go
@@ -1,55 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+import "net/url"
+import "os"
 
-type item struct {
-	name string
-}
+func main(){
 
-type character struct {
-	name     string
-	leftHand *item
-}
 
-func (c *character) pickup(i *item) {
-	if c == nil || i == nil {
-		return
+u,err:=url.Parse("https://a b.com")
+if err!=nil{
+	fmt.Println(err)
+	fmt.Printf("%#v\n",err)
+
+	if e,ok:=err.(*url.Error); ok{
+		fmt.Println("op:",e.Op)
+		fmt.Println("URL:",e.URL)
+		fmt.Println("Err:",e.Err)
 	}
-	fmt.Printf("%v picks up a %v\n", c.name, i.name)
-	c.leftHand = i
+	os.Exit(1)
 }
-
-func (c *character) give(to *character) {
-	if c == nil || to == nil {
-		return
-	}
-	if c.leftHand == nil {
-		fmt.Printf("%v has nothing to give\n", c.name)
-	}
-	if to.leftHand != nil {
-		fmt.Printf("%v 's hand is full\n", to.name)
-	}
-	to.leftHand = c.leftHand
-	c.leftHand = nil
-	fmt.Printf("%v gives %v to %v\n", c.name, to.leftHand.name, to.name)
-}
-
-func (c *character) String() string {
-	if c.leftHand == nil {
-		return fmt.Sprintf("%v is carring nothing", c.name)
-	}
-	return fmt.Sprintf("%v is carring %v", c.name, c.leftHand.name)
-}
-func main() {
-	arthur := &character{name: "Arthur"}
-	boom := &item{name: "boom!"}
-	arthur.pickup(boom)
-
-	knight := &character{name: "Knight1"}
-	arthur.give(knight)
-
-	fmt.Println(arthur)
-	fmt.Println(knight)
+fmt.Println(u)
 }
