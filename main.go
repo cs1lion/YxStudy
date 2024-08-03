1dcfa086cccb648d6e7dbb33efa261f8aa741fc6
166****4030 <18712723595@163.com>
2024-08-03 04:11:18 +0000

    7.6pointer

--- main.go
+++ main.go
@@ -3,108 +3,52 @@ package main
 
 import (
     "fmt"
-    //"encoding/json"
-    "math/rand"
-    "time"
 )
-type HoneyBee struct{
-    Name string 
+type item struct{
+    name string
 }
-func (hb HoneyBee)String()string{
-    return fmt.Sprintf("this is a %v",hb.Name)
+
+type character struct{
+    name string
+    leftHand *item
 }
-func(hb HoneyBee)Move() string{
-    switch rand.Intn(3){
-        case 0:
-            return "buzzes about"
-        default:
-            return "files to infinity and beyond"
+func (c *character)pickup(i *item){
+    if c==nil||i==nil{
+        return
     }
+    fmt.Printf("%v picks up a %v\n",c.name,i.name)
+    c.leftHand=i
 }
-func(hb HoneyBee)Eat()string{
-    switch rand.Intn(2){
-        case 0:
-            return "pollen"
-        default:
-            return "nectar"    
+
+func(c *character)give(to *character){
+    if c==nil||to==nil{
+        return
     }
-}
-
-
-type Gopher struct{
-    Name string 
-}
-func (g Gopher)String()string{
-    return fmt.Sprintf("this is a %v",g.Name)
-}
-
-func (g Gopher)Move() string{
-    switch rand.Intn(2){
-        case 0:
-            return "scurries along the ground"
-        default:
-            return "burrows in the sand"    
+    if c.leftHand==nil{
+        fmt.Printf("%v has nothing to give\n",c.name)
     }
-}
-
-func(g Gopher) Eat() string{
-    switch rand.Intn(5){
-        case 0:
-            return "carrot"
-        case 1:
-            return "lettuce"
-        case 2:
-            return "radish"
-        case 3:
-            return "corn"
-        case 4:
-            return "root"
-        default:
-            return ""                
+    if to.leftHand!=nil{
+        fmt.Printf("%v 's hand is full\n",to.name)
     }
+    to.leftHand=c.leftHand
+    c.leftHand=nil
+    fmt.Printf("%v gives %v to %v\n",c.name,to.leftHand.name,to.name)
 }
 
-type animal interface{
-    Move() string
-    Eat() string
-}
-
-func step(a animal){
-    switch rand.Intn(2){
-        case 0:
-            fmt.Printf("%v %v.\n",a,a.Move())
-        case 1:
-            fmt.Printf("%v eats %v.\n",a,a.Eat())    
+func (c *character) String()string{
+    if c.leftHand==nil{
+        return fmt.Sprintf("%v is carring nothing",c.name)
     }
+    return fmt.Sprintf("%v is carring %v",c.name,c.leftHand.name)
 }
-
-const sunrise,sunset = 8,18
-
 func main(){
-    rand.Seed(time.Now().UnixNano())
+    arthur:=&character{name:"Arthur"}     
+    boom:=&item{name:"boom!"}
+    arthur.pickup(boom)
 
-    animals:=[]animal{
-        HoneyBee{Name:"New Bee"},
-        Gopher{Name:"Go gopher"},
-    }
+    knight:=&character{name:"Knight1"}
+    arthur.give(knight)
 
-    var sol,hour int
-    loop:
-    for{
-        fmt.Printf("%2d:00 ",hour)
-        if hour<sunrise||hour>sunset{
-            fmt.Println("the animals are sleeping.")
-        }else{
-            step(animals[rand.Intn(len(animals))])
-        }
-        time.Sleep(500*time.Millisecond)
-        hour++
-        if hour>=24{
-            hour=0
-            sol++
-            if sol>=3{
-                break loop
-            }
-        }
-    }
+    fmt.Println(arthur)
+    fmt.Println(knight)
 }
